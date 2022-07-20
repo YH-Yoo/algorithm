60dca7d5ac1dc48c5f87f2e363e60f83ede53276
Yehyeon Yoo <yyoo@c5r5s5.42seoul.kr>
2022-07-20 14:29:44 +0900

    maxheqp

--- week4/maxheap.py
+++ week4/maxheap.py
@@ -2,9 +2,14 @@ import heapq
 import sys
 input = sys.stdin.readline
 
+N = int(input())
 max_heap = []
 for i in range(N):
 	x = int(input())
-	if not max_heap:
-		print(0)
+	if x == 0:
+		if not max_heap:
+			print(0)
+		else:
+			print(-1 * (heapq.heappop(max_heap)))
 	else:
+		heapq.heappush(max_heap, -x)
